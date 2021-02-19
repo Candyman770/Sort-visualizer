3cd4e97a69aed3262181723faeee36ad44da8448
Candyman770 <isaac2311.cl@gmail.com>
2021-02-20 03:11:43 +0530

    first commit

---
+++ src/sortingVisualizer/dropdown/dropdown.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {MdArrowDropDown}  from 'react-icons/md';
+
+import classes from './dropdown.module.css';
+
+const Dropdown=(props)=>{
+	return (
+		<div className={classes.Dropdown}>
+			<button>{props.name}<MdArrowDropDown/></button>
+			<ul className={classes.DropdownContent}>
+				{props.list.map((ele,id)=>{
+					return <li 
+						key={ele}
+						onClick={()=>props.clicked(ele)}
+						style={{
+							backgroundColor:props.selected===ele?'#ff3399':'white',
+							color:props.selected===ele?'white':'#ff3399'
+						}}>{ele}</li>
+				})}
+			</ul>
+		</div>
+	);
+};
+
+export default Dropdown;
